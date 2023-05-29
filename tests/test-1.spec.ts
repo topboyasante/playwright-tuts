213e6396bbdab6617bdcd454671b089aa3671528
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-05-29 13:01:25 +0100

    Progress

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,6 +1,11 @@
 import {test,expect} from '@playwright/test'
 
 test("test1",async({page})=>{
+
     //Go to the webpage you're testing:
     await page.goto("https://playwright.dev")
+
+    // Click the get started link using Locators
+    await page.getByRole('link', { name: 'Get started' }).click();
+
 })
